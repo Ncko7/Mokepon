bbb455bf6714321b19f62862d5d28dea3fb0e340
Nicolas Marroquin <niko.marroquin7@hotmail.com>
2024-04-17 20:46:12 -0500

    Agregamos el evento load a nuestro archivo js

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,6 +1,15 @@
-function seleccionarMascotaJugagor() {
- alert('seleccionaste tu mascota');
+function iniciarJuego() {
+    let botonMascota = document.getElementById('btn-mascota');
+    botonMascota.addEventListener('click', seleccionarMascotaJugagor);
 }
 
-let botonMascota = document.getElementById("btn-mascota");
-botonMascota.addEventListener("click", seleccionarMascotaJugagor);
+function seleccionarMascotaJugagor() {
+    
+    alert('seleccionaste tu mascota');
+}
+
+
+
+
+
+window.addEventListener('load', iniciarJuego)
