6b137de3c3a6eebc18ea975679cbb6e94cc95a82
Nicolas Marroquin <niko.marroquin7@hotmail.com>
2024-04-17 21:14:53 -0500

    Agregamos escuchador de eventos a nuestro proyecto

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,15 +1,34 @@
 function iniciarJuego() {
     let botonMascota = document.getElementById('btn-mascota');
     botonMascota.addEventListener('click', seleccionarMascotaJugagor);
+
 }
 
 function seleccionarMascotaJugagor() {
-    
-    alert('seleccionaste tu mascota');
+    let inputHipodoge = document.getElementById('hipodoge');
+    let inputCapipepo = document.getElementById('capipepo');
+    let inputRatigueya = document.getElementById('ratigueya');
+    let inputLangostelvis = document.getElementById('langostelvis');
+    let inputTucapalma = document.getElementById('tucapalama');
+    let inputPydos = document.getElementById('pydos');
+
+
+   if (inputHipodoge.checked) {
+    alert('Seleccionaste a hipodoge');
+   } else if (inputCapipepo.checked) {
+    alert('Seleccionaste a capipepo');
+   } else if (inputRatigueya.checked) {
+    alert('Seleccionaste a ratigueya');
+   } else if (inputLangostelvis.checked) {
+    alert('Seleccionaste a langostelvis');
+   } else if (inputTucapalma.checked) {
+    alert('Seleccionaste a tucapalama');
+   } else if (inputPydos.checked) {
+    alert('Seleccionaste a pydos');
+   } else {
+    alert('Selecciona una mascota')
+   }
 }
 
 
-
-
-
-window.addEventListener('load', iniciarJuego)
+window.addEventListener('load', iniciarJuego);
